e87bb9020baaae867a88b68e62b1b980889207ce
nooblyh <472897494@qq.com>
2021-03-28 14:04:17 +0800

    异构知识蒸馏

--- test_train.sh
+++ test_train.sh
@@ -5,10 +5,10 @@ python train.py \
     --student_type albert \
     --student_config training_configs/albert-base-v2-config.json \
     --teacher_type albert \
-    --teacher_name albert-base-v2 \
+    --teacher_name albert-xlarge-v2 \
     --alpha_ce 5.0 --alpha_mlm 2.0 --alpha_cos 1.0 --alpha_clm 0.0 --mlm \
     --freeze_pos_embs \
-    --dump_path output/my_first_training \
-    --data_file datasets/binarized_text.albert.v2.pickle \
-    --token_counts datasets/token_counts.albert.v2.pickle
+    --dump_path output/xxlarge-2048 \
+    --data_file datasets/binarized_text.albert.pickle \
+    --token_counts datasets/token_counts.albert.pickle
     # --force # overwrites the `dump_path` if it already exists.
